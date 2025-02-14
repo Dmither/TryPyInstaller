dd4316989accf11e381805b8fb9601fd9fd30e54
Dmither <mr.syvko@gmail.com>
2025-02-14 17:33:06 +0200

    update

--- reader/__main__.py
+++ reader/__main__.py
@@ -1,6 +1,8 @@
+import tkinter as tk
+
 def main():
     try:
         file1 = open("test.txt", "w")
-        file1.write("Hello!")
+        file1.write(str(tk.TkVersion))
     finally:
         file1.close()
